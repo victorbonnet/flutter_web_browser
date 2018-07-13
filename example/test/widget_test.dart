a81b666796b866aab4752eb8b1848562cdef91c9
Victor Bonnet <victor.bonnet@gmail.com>
2018-07-13 10:17:52 +0200

    Update text in test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter_test/flutter_test.dart';
 import '../lib/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Flutter Web Browser', (WidgetTester tester) async {
     await tester.pumpWidget(new MyApp());
 
     expect(find.text('Open Flutter website'), findsOneWidget);
