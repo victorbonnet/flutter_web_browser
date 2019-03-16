08009e0871c7c1b2b3d97583381a254d716213b6
Victor Bonnet <victor.bonnet@gmail.com>
2019-03-16 21:47:51 +0100

    Format lib/flutter_web_browser.dart

--- lib/flutter_web_browser.dart
+++ lib/flutter_web_browser.dart
@@ -9,12 +9,12 @@ class FlutterWebBrowser {
   static Future<dynamic> openWebPage({url, androidToolbarColor}) {
     var hexColor;
     if (androidToolbarColor != null) {
-      hexColor = '#'+androidToolbarColor.value.toRadixString(16).padLeft(8, '0');
+      hexColor =
+          '#' + androidToolbarColor.value.toRadixString(16).padLeft(8, '0');
     }
     return _channel.invokeMethod('openWebPage', {
       "url": url,
       "android_toolbar_color": hexColor,
     });
   }
-
 }
